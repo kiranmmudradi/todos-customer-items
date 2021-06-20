461e553509fe4a2c4663cef0b6eeb66cd7d8680a
kiran mudradi <kiranm.mudradi@gmail.com>
2021-06-20 12:11:20 +0530

    added new controller

---
+++ src/main/java/com/todos/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.todos.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @RequestMapping("/")
+    public String healthCheck(){
+            return "200 OK";
+    }
+}
